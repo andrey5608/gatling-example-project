aa3595f3c658e102df7527d8ea8ab57024114b7c
Andrey Petrov <andrey5608@gmail.com>
2020-04-17 23:39:24 +0300

    Change API under test to postman echo

--- src/test/scala/GatlingTest.scala
+++ src/test/scala/GatlingTest.scala
@@ -4,30 +4,31 @@ import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
 class GatlingTest extends Simulation {
-  private val baseUrl = "http://localhost:52032"
-//  private val basicAuthHeader = "Basic YmxhemU6UTF3MmUzcjQ="
-//  private val authPass = "Q1w2e3r4"
+  private val baseUrl = "https://postman-echo.com"
   private val contentType = "text/plain"
-  private val endpoint = "/api/v2/sensors"
-  private val requestCount = 10000
+  private val endpoint = "/get?foo1=bar1&foo2=bar2"
+  private val usersCount = 10
 
 
   val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(baseUrl)
     .inferHtmlResources()
     .acceptHeader("*/*")
-    //.authorizationHeader(basicAuthHeader)
     .contentTypeHeader(contentType)
     .userAgentHeader("curl/7.54.0")
+    .disableWarmUp
+    .disableCaching
 
   val headers_0 = Map("Expect" -> "100-continue")
 
   val scn: ScenarioBuilder = scenario("RecordedSimulation")
-    .exec(http("request_0")
+    .repeat(10)
+    {
+      exec(http("request_0")
       .get(endpoint)
       .headers(headers_0)
-      //.basicAuth(authUser, authPass)
       .check(status.is(200)))
+    }
 
-  setUp(scn.inject(atOnceUsers(requestCount))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(usersCount))).protocols(httpProtocol)
 }
