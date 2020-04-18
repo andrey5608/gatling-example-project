05f1c0ab4cbdbcb854fef80d3186346f5a54aa94
Andrey Petrov <andrey5608@gmail.com>
2020-04-19 00:09:32 +0300

    Added another one test + modified existing
    Put headers into json file.
    Turned on requests and responses logging settings.
    Add one new scenario.

--- src/test/scala/GatlingTest.scala
+++ src/test/scala/GatlingTest.scala
@@ -4,12 +4,6 @@ import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
 class GatlingTest extends Simulation {
-  private val baseUrl = "https://postman-echo.com"
-  private val contentType = "text/plain"
-  private val endpoint = "/get?foo1=bar1&foo2=bar2"
-  private val usersCount = 10
-
-
   val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(baseUrl)
     .inferHtmlResources()
@@ -18,17 +12,23 @@ class GatlingTest extends Simulation {
     .userAgentHeader("curl/7.54.0")
     .disableWarmUp
     .disableCaching
-
   val headers_0 = Map("Expect" -> "100-continue")
-
   val scn: ScenarioBuilder = scenario("RecordedSimulation")
-    .repeat(10)
-    {
+    .repeat(10) {
       exec(http("request_0")
-      .get(endpoint)
-      .headers(headers_0)
-      .check(status.is(200)))
+        .get(endpoint)
+        .headers(headers_0)
+        .check(status.is(200)))
     }
+  private val baseUrl = "https://postman-echo.com"
+  private val contentType = "text/plain"
+  private val endpoint = "/get?foo1=bar1&foo2=bar2"
+  private val usersCount = 10
 
-  setUp(scn.inject(atOnceUsers(usersCount))).protocols(httpProtocol)
+  setUp(
+    scn.inject(
+      nothingFor(4),
+      atOnceUsers(5), //Initial: 5 users
+      rampUsers(200) during 10
+    ).protocols(httpProtocol))
 }
---
+++ src/test/scala/GetEmployeesListTest.scala
@@ -0,0 +1,46 @@
+import io.gatling.core.Predef._
+import io.gatling.core.body.Body
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+import scala.util.parsing.json.JSON
+
+class GetEmployeesListTest extends Simulation {
+  val jsonHeaders: String = scala.io.Source.fromFile("./src/test/resources/headers.json").mkString
+  val jsonMap = JSON.parseFull(jsonHeaders).getOrElse(0).asInstanceOf[Map[String, String]]
+  val innerMap = jsonMap.asInstanceOf[Map[String, String]]
+  val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl(baseUrl)
+    .inferHtmlResources()
+    .acceptHeader("*/*")
+    .contentTypeHeader(contentType)
+    .maxConnectionsPerHostLikeChrome
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
+    .disableCaching
+  val headers_0 = Map("Authorization" -> "Bearer %s".format(innerMap("Bearer")),
+    "Id-Token" -> innerMap("Id-Token"),
+    "Origin" -> innerMap("Origin"),
+    "Referrer" -> innerMap("Referrer"),
+    "Content-Type" -> "application/json")
+  val requestBody: Body = StringBody(
+    "{put here json body}")
+  val scn: ScenarioBuilder = scenario("RecordedSimulation")
+    .repeat(120) {
+      exec(http("/employee-spr/query")
+        .post(endpoint)
+        .body(requestBody)
+        .headers(headers_0)
+        .check(status.is(200)))
+    }
+  innerMap.keys //will give keys
+  private val baseUrl = "https://server-url"
+  private val contentType = "text/plain"
+  private val endpoint = "/query-part"
+  private val usersCount = 10
+
+  setUp(
+    scn.inject(
+      nothingFor(4),
+      atOnceUsers(usersCount)
+    ).protocols(httpProtocol))
+}
